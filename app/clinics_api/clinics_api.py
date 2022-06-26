df8818938f90204809aac67ed21582c17fc262c3
MrBass <belbizri@projexia.ca>
2022-06-25 21:43:11 -0400

    as

--- app/clinics_api/clinics_api.py
+++ app/clinics_api/clinics_api.py
@@ -2,10 +2,10 @@ from flask import Blueprint, request, jsonify
 from web_util import assert_data_has_keys
 from clinics.clinic import Clinic
 
-clinic_api = Blueprint('clinics_api', __name__, url_prefix='/api/clinics')
+get_clinics = Blueprint('clinics_api', __name__, url_prefix='/api/clinics')
 
 
-@clinic_api.route('/get_clinics', methods=['GET'])
+@get_clinics.route('/get_clinics', methods=['GET'])
 def sync():
     clinic =  Clinic.get_all_clinic()
     print(clinic)
