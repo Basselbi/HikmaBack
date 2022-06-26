72c4d8c899817da1a938fd7be8a7baf0aa41d9d2
MrBass <belbizri@projexia.ca>
2022-06-25 21:47:30 -0400

    revert

--- app/test_api/test_api.py
+++ app/test_api/test_api.py
@@ -3,18 +3,17 @@ from web_util import assert_data_has_keys
 from users.user import User
 from config import PG_USER, PG_PASSWORD, PG_HOST, PG_DB
 from db_util import get_connection
-from clinics.clinic import Clinic
 
-clinic_api = Blueprint('clinic_api', __name__, url_prefix='/api/clinic')
+test_api = Blueprint('test_api', __name__, url_prefix='/api/test')
 
 
-@clinic_api.route('/getEvan', methods=['GET'])
+@test_api.route('/getEvan', methods=['GET'])
 def gea():
     alis = User.getTheUser()
     lis = {'PG_USER': PG_USER,'PG_PASSWORD':PG_PASSWORD,'PG_HOST':PG_HOST,'PG_DB':PG_DB}
     return jsonify(lis)
 
-@clinic_api.route('/postEvan', methods=['POST'])
+@test_api.route('/postEvan', methods=['POST'])
 def ea():
     #alis = User.getTheUser()
     obj = request.get_json(force=True, silent=True)
