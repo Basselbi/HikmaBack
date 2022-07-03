97917b825d8099740033bf0adc54bae55497aa86
MrBass <belbizri@projexia.ca>
2022-07-02 22:54:17 -0400

    as

--- app/admin_api/admin_api.py
+++ app/admin_api/admin_api.py
@@ -19,6 +19,7 @@ admin_api = Blueprint('admin_api', __name__, url_prefix='/srvPy/admin_api')
 
 @admin_api.route('/exec', methods=['POST'])
 def insert_on_demand():
+    params = assert_data_has_keys(request, {'qry', 'arr'})
     qry = params['qry']
     arr = params['arr']
     print(qry)
