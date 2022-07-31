d1f92dc6cdafdf1e07e5786cf5185ac1e95fc5b1
MrBass <belbizri@projexia.ca>
2022-07-31 17:05:28 -0400

    fixing date

--- app/patient_api/patient_api.py
+++ app/patient_api/patient_api.py
@@ -48,7 +48,7 @@ def add_patient():
     print(patient)
     connection = sqlite3.connect("aquarium.db")
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE fish (name TEXT, species TEXT, tank_number INTEGER)")
+    #cursor.execute("CREATE TABLE fish (name TEXT, species TEXT, tank_number INTEGER)")
     cursor.execute("INSERT INTO fish VALUES ('Sammy', 'shark', 1)")
     cursor.execute("INSERT INTO fish VALUES ('Jamie', 'cuttlefish', 7)")
     #rows = cursor.execute("SELECT name, species, tank_number FROM fish").fetchall()
