1692ca608a1a5525fa96479a47b353d9ea183796
MrBass <belbizri@projexia.ca>
2022-07-31 15:56:43 -0400

    fixing date

--- app/patients/data_access.py
+++ app/patients/data_access.py
@@ -5,24 +5,24 @@ from language_strings.language_string import to_id, LanguageString
 
 
 def add_patient(patient: Patient):
-    # update_language_string(patient.given_name)
-    # update_language_string(patient.surname)
-    # update_language_string(patient.country)
-    # update_language_string(patient.hometown)
+    update_language_string(patient.given_name)
+    update_language_string(patient.surname)
+    update_language_string(patient.country)
+    update_language_string(patient.hometown)
     with get_connection() as conn:
         with conn.cursor() as cur:
             cur.execute('''INSERT INTO patients (id, given_name, surname, date_of_birth, sex, country, hometown, phone, edited_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
                         ON CONFLICT (id)
                         DO UPDATE SET (id, given_name, surname, date_of_birth, sex, country, hometown, phone, edited_at) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)''',
                         [patient.id,
-                        patient.given_name,
-                        patient.surname,
-                        patient.date_of_birth,
-                        patient.sex,
-                        patient.country,
-                        patient.hometown,
-                        patient.phone,
-                        patient.edited_at
+                         to_id(patient.given_name),
+                         to_id(patient.surname),
+                         patient.date_of_birth,
+                         patient.sex,
+                         to_id(patient.country),
+                         to_id(patient.hometown),
+                         patient.phone,
+                         patient.edited_at
                          ])
 
 
