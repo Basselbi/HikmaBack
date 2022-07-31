766745d7830023039ceab64bc56d9259d99a46d4
MrBass <belbizri@projexia.ca>
2022-07-31 14:56:30 -0400

    adding upsert for patient

--- app/patients/data_access.py
+++ app/patients/data_access.py
@@ -11,7 +11,9 @@ def add_patient(patient: Patient):
     update_language_string(patient.hometown)
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute('INSERT INTO patients (id, given_name, surname, date_of_birth, sex, country, hometown, phone, edited_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)',
+            cur.execute('''INSERT INTO patients (id, given_name, surname, date_of_birth, sex, country, hometown, phone, edited_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        ON CONFLICT (id)
+                        DO UPDATE SET (id, given_name, surname, date_of_birth, sex, country, hometown, phone, edited_at) values (%s,%s,%s, %s, %s, %s, %s,%s, %s, %s, %s)''',
                         [patient.id,
                          to_id(patient.given_name),
                          to_id(patient.surname),
@@ -61,7 +63,6 @@ def patient_from_key_data(given_name: str, surname: str, country: str, sex: str)
             if row is None:
                 return None
             return row[0]
-
 
 def all_patient_data():
     query = """
